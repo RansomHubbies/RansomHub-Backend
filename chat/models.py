8c9a3bc72ac7982fa323bff64bb25f18dd05eabe
Manav Mittal <Manav Mittal>
2025-03-28 22:46:00 +0530

    updated settings.py

--- chat/models.py
+++ chat/models.py
@@ -1,15 +1,23 @@
-# from django.db import models
-# from django.contrib.auth.models import User
+from django.db import models
+from users.models import CustomUser
 
-# class ChatChannel(models.Model):
-#     name = models.CharField(max_length=255, unique=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
+# create a model to store the messages between users
+class Message(models.Model):
+    
+    sender = models.CharField(max_length=255)
+    recipient = models.CharField(max_length=255)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
-# class Message(models.Model):
-#     channel = models.ForeignKey(ChatChannel, on_delete=models.CASCADE)
-#     sender = models.ForeignKey(User, on_delete=models.CASCADE)
-#     content = models.TextField()  # This can be encrypted content
-#     timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.sender}: {self.message}"
 
-#     def __str__(self):
-#         return f"{self.sender.username}: {self.content}"
+# create a model to store the group information
+class Group(models.Model):
+    
+    username = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+    members = models.ManyToManyField(CustomUser)
+
+    def __str__(self):
+        return self.name
--- chat/views.py
+++ chat/views.py
@@ -4,6 +4,8 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import status
 from users.models import CustomUser
 from .messenger import pusher_client
+from .models import Message, Group
+import hashlib
 
 @api_view(['POST'])
 @permission_classes([AllowAny]) # Change to IsAuthenticated after testing
@@ -45,6 +47,39 @@ def send_message(request):
     except CustomUser.DoesNotExist:
         return Response({"error": "Invalid sender or recipient"}, status=status.HTTP_400_BAD_REQUEST)
 
+    except Exception as e:
+        print(e)
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def create_group(request):
+    """
+    Create a group
+    """
+    try:
+        # Get the group name and members
+        group_name = request.data.get('name')
+        members_usernames = request.data.get('members')
+
+        # members is a list of usernames
+        members = CustomUser.objects.filter(username__in=members_usernames)
+
+        if not members:
+            return Response({"error": "Invalid member"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        base_string = group_name + "".join(members_usernames)
+        username = hashlib.sha256(base_string.encode()).hexdigest()[:12]
+
+        group = Group.objects.create(username=username, name=group_name)
+        group.members.set(members)
+        group.save()
+
+        return Response({"message": "Group created"}, status=status.HTTP_201_CREATED)
+    
+    except CustomUser.DoesNotExist:
+        return Response({"error": "Invalid member"}, status=status.HTTP_400_BAD_REQUEST)
+    
     except Exception as e:
         print(e)
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
