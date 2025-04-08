4c400b02b45ca743a5f1437987a63446820e5c3d
Manav Mittal <Manav Mittal>
2025-04-08 14:17:13 +0530

    changes media and static root

--- backend/settings.py
+++ backend/settings.py
@@ -156,8 +156,10 @@ MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #production
-# STATIC_ROOT = "/var/www/RansomHub-Backend/static/"  
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # For local development
+STATIC_ROOT = "/var/www/RansomHub-Backend/static/"  
+
+# Development
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # For local development
 
 
 
@@ -166,8 +168,10 @@ CORS_ALLOW_CREDENTIALS = True  # For cross-domain cookies
 CORS_EXPOSE_HEADERS = ['Content-Type', 'Authorization']
 
 #Production
-# MEDIA_ROOT = "/var/www/RansomHub-Backend/media/"  
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # For local development
+MEDIA_ROOT = "/var/www/RansomHub-Backend/media/"  
+
+# Development
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # For local development
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
